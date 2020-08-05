3c50cdbbdc9833cc63fff0dbaa37aa28a10dc5b4
Yang Xu <35667575+ImXman@users.noreply.github.com>
2020-08-05 16:31:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="KACA",
+    name="MACA",
     version="0.0.1",
     author="Yang Xu",
     author_email="yxu71@vols.utk.edu",
-    description="KACA: Knowledge-based automatic cell-type annotation for single cell expression data",
+    description="MACA: Marker-based automatic cell-type annotation for single cell expression data",
     #long_description=long_description,
     #long_description_content_type="text/markdown",
-    url="https://github.com/ImXman/KACA",
+    url="https://github.com/ImXman/MACA",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
