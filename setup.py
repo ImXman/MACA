c8b1d323410cef6b43a0193d2ee99f11075613dc
Yang Xu <35667575+ImXman@users.noreply.github.com>
2021-06-13 15:21:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@ setuptools.setup(
     url="https://github.com/ImXman/MACA",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPLv3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
